ef17900eafc6e3cb37914a569b17d4f42b3f8ded
zawkindev <zawkindev@gamil.com>
2023-07-30 17:34:55 +0500

    language drop-down function added

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     const languageActiveButton = document.getElementById('languageActiveButton');
+    const languageInActiveButtons = document.getElementById('languageInactiveButtons');
     const languageButtons = document.getElementsByClassName('inActiveLanguageButton');
     let languages = ["English", "Русский", "O'zbek"];
-
+    let languageDropdown = false
     for (let index = 0; index < languageButtons.length; index++) {
         const button = languageButtons[index];
         const activeLanguage = languages[0]
@@ -13,8 +14,12 @@ document.addEventListener('DOMContentLoaded', function () {
             languages[0] = clickedLanguage;
             languages[lang] = activeLanguage;
             languageActiveButton.getElementsByTagName('span')[0].innerHTML = clickedLanguage;
-
+            languageDropdown = false;
+            languageInActiveButtons.style.display = 'none';
         });
-
     }
+    languageActiveButton.addEventListener('click', function () {
+        languageDropdown = !languageDropdown;
+        languageInActiveButtons.style.display = (languageDropdown? 'flex' : 'none');
+    });
 })
