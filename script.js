8382a60875535778bc94d6b1348b4fedd5ea265c
zawkindev <zawkindev@gamil.com>
2023-07-31 20:38:14 +0500

    more menu fixed

--- script.js
+++ script.js
@@ -5,30 +5,34 @@ document.addEventListener('DOMContentLoaded', function () {
     const languageButtons = document.getElementsByClassName('inActiveLanguageButton');
     const moreDropdownMenuOptions = document.getElementsByClassName('moreMenuOptions')[0];
     const more = document.getElementById('more');
+
     let languages = ["English", "Русский", "O'zbek"];
     let languageDropdown = false
-    for (let index = 0; index < languageButtons.length; index++) {
-        const button = languageButtons[index];
-        const activeLanguage = languages[0]
-        const clickedLanguage = button.innerText;
-        button.addEventListener('click', function () {
-            const lang = languages.indexOf(clickedLanguage);
-            languages[0] = clickedLanguage;
-            languages[lang] = activeLanguage;
-            languageActiveButton.getElementsByTagName('span')[0].innerHTML = clickedLanguage;
-            languageDropdown = false;
-            languageInActiveButtons.style.display = 'none';
-        });
-    }
-    languageActiveButton.addEventListener('click', function () {
-        languageDropdown = !languageDropdown;
-        languageInActiveButtons.style.display = (languageDropdown ? 'flex' : 'none');
-    });
-
-
+    let moreMenuLoggic = false;
+    // for (let index = 0; index < languageButtons.length; index++) {
+    //     const button = languageButtons[index];
+    //     const activeLanguage = languages[0]
+    //     const clickedLanguage = button.innerText;
+    //     button.addEventListener('click', function () {
+    //         const lang = languages.indexOf(clickedLanguage);
+    //         languages[0] = clickedLanguage;
+    //         languages[lang] = activeLanguage;
+    //         languageActiveButton.getElementsByTagName('span')[0].innerHTML = clickedLanguage;
+    //         languageDropdown = false;
+    //         languageInActiveButtons.style.display = 'none';
+    //     });
+    // }
+    // languageActiveButton.addEventListener('click', function () {
+    //     languageDropdown = !languageDropdown;
+    //     languageInActiveButtons.style.display = (languageDropdown ? 'flex' : 'none');
+    // });
 
     more.addEventListener('mouseover', function () {
         moreDropdownMenu.style.display = 'flex';
+    });
+    more.addEventListener('click', function () {
+        moreMenuLoggic = !moreMenuLoggic;
+        moreDropdownMenu.style.display = moreMenuLoggic? 'flex' : 'none';
     });
     moreDropdownMenuOptions.addEventListener('mouseout', function () {
         moreDropdownMenu.style.display = 'none';
@@ -37,7 +41,7 @@ document.addEventListener('DOMContentLoaded', function () {
         moreDropdownMenu.style.display = 'flex';
     })
 
-
-
+    console.log(moreDropdownMenu);
+    console.log(moreDropdownMenu);
 })
 
